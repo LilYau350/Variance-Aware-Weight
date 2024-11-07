6c8235101d1950d81f530b02555a0b165ea3be48
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2024-11-07 19:49:32 +0800

    Update _init_.py

--- models/_init_.py
+++ models/_init_.py
@@ -6,8 +6,8 @@ from .dit import DiT_S, DiT_B, DiT_L, DiT_XL
 from .uvit import UViT_S, UViT_S_D, UViT_M, UViT_L, UViT_H
 
 __all__ = [
-    'UNet_32', 'UNet_64', 'UNet_128', 'UNet_256', 'UNet_512',
-    'ViT_S', 'ViT_B', 'ViT_L', 'ViT_XL',
-    'DiT_S', 'DiT_B', 'DiT_L', 'DiT_XL',
-    'UViT_S', 'UViT_S_D', 'UViT_M', 'UViT_L', 'UViT_H'
+    "UNet-32","ADM-32", "ADM-64", "ADM-128", "ADM-256", "ADM-512", "UNet-64", "LDM",
+    "ViT-S", "ViT-B", "ViT-L", "ViT-XL",
+    "DiT-S", "DiT-B", "DiT-L", "DiT-XL",
+    "UViT-S", "UViT-S-D", "UViT-M", "UViT-L", "UViT-H"
 ]
