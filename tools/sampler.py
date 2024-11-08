2fa0db95604b22b9bd6dc45448dffa17d9003dff
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2024-11-08 22:34:18 +0800

    Update sampler.py

--- tools/sampler.py
+++ tools/sampler.py
@@ -6,17 +6,14 @@ from tools import dist_util
 from .cfg_edm import ablation_sampler, float_equal, Net
 from models.unet import EncoderUNetModel
 
-class Sampler:
-    def __init__(self, args, device, model, ema_model, diffusion):
+class Classifier:
+    def __init__(self, args, device, model):
         self.args = args
-        self.device = device        
+        self.device = device
         self.model = model
-        self.ema_model = ema_model        
-        self.diffusion = diffusion
-        self.classifier = self.load_classifier() if args.use_classifier else None
+        self.classifier = self._load_classifier() if args.use_classifier else None
 
-
-    def create_classifier(self):
+    def _create_classifier(self):
         attention_ds = [self.args.image_size // res for res in self.model.attention_resolutions]
         classifier_kwargs = {
             'image_size': self.model.image_size,
@@ -33,67 +30,63 @@ class Sampler:
         }
         return EncoderUNetModel(**classifier_kwargs)
 
-    def load_classifier(self):
-        classifier = self.create_classifier()
+    def _load_classifier(self):
+        classifier = self._create_classifier()
         classifier.load_state_dict(torch.load(self.args.use_classifier, map_location="cpu"))
         classifier.to(self.device)
         classifier.eval()
         return classifier
 
-    def sync_ema_model(self):
-        """Synchronize EMA model parameters across distributed devices."""
-        for param in self.ema_model.parameters():
-            dist.broadcast(param.data, src=0)
-
-    def cond_fn(self, x, t, y=None, classifier_scale=1.0):
+    def cond_fn(self, x, t, y, scale=1.0):
         assert y is not None
         with torch.enable_grad():
             x_in = x.detach().requires_grad_(True)
             logits = self.classifier(x_in, t)
             log_probs = torch.nn.functional.log_softmax(logits, dim=-1)
             selected = log_probs[range(len(log_probs)), y.view(-1)]
-            return torch.autograd.grad(selected.sum(), x_in)[0] * classifier_scale
+            return torch.autograd.grad(selected.sum(), x_in)[0] * scale
 
-    def model_fn(self, x, t, y=None):
-        return self.model(x, t, y if self.args.class_cond else None)
+
+def sync_ema_model(eval_model):
+    """Synchronize EMA model parameters across distributed devices."""
+    for param in eval_model.parameters():
+        dist.broadcast(param.data, src=0)
+
+class Sampler:
+    def __init__(self, args, device, eval_model, diffusion, classifier=None):
+        self.args = args
+        self.device = device
+        self.eval_model = eval_model
+        self.diffusion = diffusion
+        self.classifier = classifier
+
+    def _model_fn(self, x, t, y=None):
+        return self.eval_model(x, t, y if self.args.class_cond else None)
 
     def ddim_sampler(self, num_samples, sample_size, image_size, num_classes, progress_bar=False):
-        self.model.eval()
+        self.eval_model.eval()
         all_samples, all_labels = [], []
         world_size = dist.get_world_size() if self.args.parallel else 1
-        
+
         if self.args.parallel:
-            self.sync_ema_model()
+            sync_ema_model(self.eval_model)
             dist.barrier()
 
         if progress_bar and dist_util.is_main_process():
             pbar = tqdm(total=num_samples, desc="Generating Samples (DDIM)")
 
         while len(all_samples) * sample_size < num_samples:
-            classes = torch.randint(low=0, high=num_classes, size=(sample_size,), device=self.device) if self.args.class_cond else None
+            classes = torch.randint(0, num_classes, (sample_size,), device=self.device) if self.args.class_cond else None
             sample = self.diffusion.ddim_sample_loop(
-                self.model if not self.classifier else self.model_fn,
+                self.eval_model if not self.classifier else self._model_fn,
                 (sample_size, 3, image_size, image_size),
                 device=self.device,
                 model_kwargs={"y": classes} if self.args.class_cond else {},
-                cond_fn=(lambda x, t, y: self.cond_fn(x, t, y, self.args.guidance_scale)) if self.classifier else None,
+                cond_fn=(lambda x, t, y: self.classifier.cond_fn(x, t, y, self.args.guidance_scale)) if self.classifier else None,
             )
-            sample = ((sample + 1) * 127.5).clamp(0, 255).to(torch.uint8)
-            sample = sample.permute(0, 2, 3, 1).contiguous()
+            sample = ((sample + 1) * 127.5).clamp(0, 255).to(torch.uint8).permute(0, 2, 3, 1).contiguous()
 
-            if self.args.parallel:
-                gathered_samples = [torch.zeros_like(sample) for _ in range(world_size)]
-                dist.all_gather(gathered_samples, sample)
-                all_samples.extend([sample.cpu().numpy() for sample in gathered_samples])
-
-                if self.args.class_cond:
-                    gathered_labels = [torch.zeros_like(classes) for _ in range(world_size)]
-                    dist.all_gather(gathered_labels, classes)
-                    all_labels.extend([labels.cpu().numpy() for labels in gathered_labels])
-            else:
-                all_samples.append(sample.cpu().numpy())
-                if self.args.class_cond:
-                    all_labels.append(classes.cpu().numpy())
+            self._gather_samples(all_samples, all_labels, sample, classes, world_size)
 
             if dist_util.is_main_process() and progress_bar:
                 pbar.update(sample_size * world_size)
@@ -101,66 +94,70 @@ class Sampler:
         return all_samples, all_labels
 
     def heun_sampler(self, num_samples, sample_size, image_size, num_classes, progress_bar=False):
-        self.model.eval()
+        self.eval_model.eval()
         all_samples, all_labels = [], []
         world_size = dist.get_world_size() if self.args.parallel else 1
-        
+
         if self.args.parallel:
-            self.sync_ema_model()
+            sync_ema_model(self.eval_model)
             dist.barrier()
 
         if progress_bar and dist_util.is_main_process():
             pbar = tqdm(total=num_samples, desc="Generating Samples (Heun)")
 
-        if self.args.in_chans == 4:
-            vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse").to(self.device)
-
-        net = Net(model=self.model, img_channels=self.args.in_chans, img_resolution=image_size,
+        vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse").to(self.device) if self.args.in_chans == 4 else None
+        net = Net(model=self.eval_model, img_channels=self.args.in_chans, img_resolution=image_size,
                   noise_schedule=self.args.beta_schedule, amp=self.args.amp).to(self.device)
 
         while len(all_samples) * sample_size < num_samples:
-            y_cond = torch.randint(low=0, high=num_classes, size=(sample_size,), device=self.device) if self.args.class_cond else None
+            y_cond = torch.randint(0, num_classes, (sample_size,), device=self.device) if self.args.class_cond else None
             z = torch.randn([sample_size, net.img_channels, net.img_resolution, net.img_resolution], device=self.device)
-            if not float_equal(self.args.guidance_scale, 1.0):
-                z = torch.cat((z, z), dim=0)
-                y_uncond = torch.randint(low=num_classes, high=num_classes + 1, size=(sample_size,), device=self.device)
-                class_labels = torch.cat((y_cond, y_uncond), dim=0) if y_cond is not None else None
-            else:
-                class_labels = y_cond
+            class_labels, z = self._prepare_labels(y_cond, num_classes, sample_size, z)
 
             sample = ablation_sampler(net, latents=z, num_steps=self.args.sample_timesteps, solver="heun",
                                       class_labels=class_labels, guidance_scale=self.args.guidance_scale,
                                       eps_scaler=self.args.eps_scaler)
-            
-            if self.args.in_chans == 4:
-                if not float_equal(self.args.guidance_scale, 1.0):
-                    sample, _ = sample.chunk(2, dim=0)
-                sample = vae.decode(sample.float() / 0.18215).sample
-
-            sample = ((sample + 1) * 127.5).clamp(0, 255).to(torch.uint8)
-            sample = sample.permute(0, 2, 3, 1).contiguous()
-
-            if self.args.parallel:
-                gathered_samples = [torch.zeros_like(sample) for _ in range(world_size)]
-                dist.all_gather(gathered_samples, sample)
-                all_samples.extend([sample.cpu().numpy() for sample in gathered_samples])
-
-                if self.args.class_cond:
-                    gathered_labels = [torch.zeros_like(class_labels) for _ in range(world_size)]
-                    dist.all_gather(gathered_labels, class_labels)
-                    all_labels.extend([labels.cpu().numpy() for labels in gathered_labels])
-            else:
-                all_samples.append(sample.cpu().numpy())
-                if self.args.class_cond:
-                    all_labels.append(class_labels.cpu().numpy())
+            sample = self._process_sample(sample, vae)
+            self._gather_samples(all_samples, all_labels, sample, class_labels, world_size)
 
             if dist_util.is_main_process() and progress_bar:
                 pbar.update(sample_size * world_size)
-        
+
         return all_samples, all_labels
 
-    def sample(self, num_samples, sample_size, image_size, num_classes, method="ddim", progress_bar=False):
-        if method == "ddim":
+    def _gather_samples(self, all_samples, all_labels, sample, labels, world_size):
+        """Gather samples across devices if running in parallel."""
+        if self.args.parallel:
+            gathered_samples = [torch.zeros_like(sample) for _ in range(world_size)]
+            dist.all_gather(gathered_samples, sample)
+            all_samples.extend([sample.cpu().numpy() for sample in gathered_samples])
+            if self.args.class_cond:
+                gathered_labels = [torch.zeros_like(labels) for _ in range(world_size)]
+                dist.all_gather(gathered_labels, labels)
+                all_labels.extend([label.cpu().numpy() for label in gathered_labels])
+        else:
+            all_samples.append(sample.cpu().numpy())
+            if self.args.class_cond:
+                all_labels.append(labels.cpu().numpy())
+
+    def _prepare_labels(self, y_cond, num_classes, sample_size, z):
+        """Prepare conditional and unconditional labels based on guidance scale."""
+        if not float_equal(self.args.guidance_scale, 1.0):
+            z = torch.cat((z, z), dim=0)
+            y_uncond = torch.randint(num_classes, num_classes + 1, (sample_size,), device=self.device)
+            return torch.cat((y_cond, y_uncond), dim=0), z 
+        return y_cond, z
+
+    def _process_sample(self, sample, vae):
+        """Process and decode sample if using VAE."""
+        if vae:
+            if not float_equal(self.args.guidance_scale, 1.0):
+                sample, _ = sample.chunk(2, dim=0)
+            sample = vae.decode(sample.float() / 0.18215).sample
+        return ((sample + 1) * 127.5).clamp(0, 255).to(torch.uint8).permute(0, 2, 3, 1).contiguous()
+
+    def sample(self, num_samples, sample_size, image_size, num_classes, progress_bar=False):
+        if self.args.sampler == "ddim":
             return self.ddim_sampler(num_samples, sample_size, image_size, num_classes, progress_bar)
-        elif method == "heun":
+        elif self.args.sampler == "heun":
             return self.heun_sampler(num_samples, sample_size, image_size, num_classes, progress_bar)
