6733547b814f0d8c0f8291f975dac967c4883a1e
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2024-11-19 11:50:12 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data'
           --in_chans 3 --image_size 32 --num_classes 10 --model 'UNet-32' \
           --lr 1e-4 --betas 0.9 0.999 --dropout 0.1 --drop_label_prob 0.1 --total_steps 200000 --batch_size 128 \
           --beta_schedule 'cosine' --loss_type 'MSE' --weight_type 'min_snr_5' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp False \
+          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp True \
           --sample_timesteps 10 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 50000 --eval_step 25000 \
           --fid_cache './stats/fid_stats_cifar_train.npz' 
 
@@ -17,7 +17,7 @@ torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data'
           --in_chans 3 --image_size 32 --num_classes 10 --model 'UNet-32' \
           --lr 1e-4 --betas 0.9 0.999 --dropout 0.1 --drop_label_prob 0.1 --total_steps 200000 --batch_size 128 \
           --beta_schedule 'linear' --loss_type 'MSE' --weight_type 'constant' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp False \
+          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp True \
           --sample_timesteps 10 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 50000 --eval_step 25000 \
           --fid_cache './stats/fid_stats_cifar_train.npz' 
 
@@ -26,7 +26,7 @@ torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data'
           --in_chans 3 --image_size 32 --num_classes 10 --model 'UNet-32' \
           --lr 1e-4 --betas 0.9 0.999 --dropout 0.1 --drop_label_prob 0.1 --total_steps 200000 --batch_size 128 \
           --beta_schedule 'cosine' --loss_type 'MSE' --weight_type 'constant' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp False \
+          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp True \
           --sample_timesteps 10 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 50000 --eval_step 25000 \
           --fid_cache './stats/fid_stats_cifar_train.npz' 
 
@@ -44,7 +44,7 @@ torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data'
           --in_chans 3 --image_size 32 --num_classes 10 --model 'UNet-32' \
           --lr 1e-4 --betas 0.9 0.999 --dropout 0.1 --drop_label_prob 0.1 --total_steps 200000 --batch_size 128 \
           --beta_schedule 'linear' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp False \
+          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp True \
           --sample_timesteps 10 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 50000 --eval_step 25000 \
           --fid_cache './stats/fid_stats_cifar_train.npz' 
 
@@ -53,7 +53,7 @@ torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data'
           --in_chans 3 --image_size 32 --num_classes 10 --model 'UNet-32' \
           --lr 1e-4 --betas 0.9 0.999 --dropout 0.1 --drop_label_prob 0.1 --total_steps 200000 --batch_size 128 \
           --beta_schedule 'cosine' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp False \
+          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp True \
           --sample_timesteps 10 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 50000 --eval_step 25000 \
           --fid_cache './stats/fid_stats_cifar_train.npz' 
 
@@ -75,14 +75,16 @@ torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data'
 torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'CelebA' \
           --in_chans 3 --image_size 64 --num_classes 0 --model 'UNet-64' \
           --lr 1e-4 --betas 0.9 0.999 --dropout 0.0 --drop_label_prob 0.0 --total_steps 500000 --batch_size 128 \
-          --warmup_steps 5000 --cosine_decay False --class_cond False --parallel True --amp False \
+          --beta_schedule 'optim_3' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
+          --warmup_steps 5000 --cosine_decay False --class_cond False --parallel True --amp True \
           --sample_timesteps 20 --guidance_scale 1.0 --eps_scaler 1.0 --save_step 100000 --eval_step 100000 --fid_cache '' \
 
 # CelebA 64×64×3 ViT
 torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'CelebA' \
-          --patch_size 4 --in_chans 3 --image_size 64 --num_classes 0 --model 'ViT-S' \
+          --patch_size 2 --in_chans 3 --image_size 64 --num_classes 0 --model 'ViT-S' \
           --lr 1e-4 --betas 0.99 0.99 --dropout 0.0 --drop_label_prob 0.0 --total_steps 500000 --batch_size 128 \
-          --warmup_steps 5000 --cosine_decay False --class_cond False --parallel True --amp False \
+          --beta_schedule 'optim_3' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
+          --warmup_steps 5000 --cosine_decay False --class_cond False --parallel True --amp True \
           --sample_timesteps 20 --guidance_scale 1.0 --eps_scaler 1.0 --save_step 100000 --eval_step 100000 --fid_cache '' \
 
 ################################################################################################################################
