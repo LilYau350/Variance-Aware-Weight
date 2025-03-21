d1e5cd9b05aafafcbea7d126afb4deac31860882
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2025-03-21 16:47:17 +0800

    Update _init_.py

--- models/_init_.py
+++ models/_init_.py
@@ -3,11 +3,13 @@
 from .unet import UNet_32, UNet_64, UNet_128, UNet_256, UNet_512
 from .vit import ViT_S, ViT_B, ViT_L, ViT_XL
 from .dit import DiT_S, DiT_B, DiT_L, DiT_XL
+from .sit import SiT_S, SiT_B, SiT_L, SiT_XL
 from .uvit import UViT_S, UViT_S_D, UViT_M, UViT_L, UViT_H
 
 __all__ = [
-    "UNet-32","ADM-32", "ADM-64", "ADM-128", "ADM-256", "ADM-512", "UNet-64", "LDM",
-    "ViT-S", "ViT-B", "ViT-L", "ViT-XL",
-    "DiT-S", "DiT-B", "DiT-L", "DiT-XL",
-    "UViT-S", "UViT-S-D", "UViT-M", "UViT-L", "UViT-H"
+    'UNet_32', 'UNet_64', 'UNet_128', 'UNet_256', 'UNet_512',
+    'ViT_S', 'ViT_B', 'ViT_L', 'ViT_XL',
+    'DiT_S', 'DiT_B', 'DiT_L', 'DiT_XL',
+    'SiT_S', 'SiT_B', 'SiT_L', 'SiT_XL',
+    'UViT_S', 'UViT_S_D', 'UViT_M', 'UViT_L', 'UViT_H'
 ]
