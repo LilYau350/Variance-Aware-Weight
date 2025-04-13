c255a60aa58671b7edac07a6a4881ef4b9030791
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2025-04-14 02:02:38 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,90 +1,31 @@
 #!/bin/bash
 
 ################################################################################################################################
-# Ablation study in CIFAR-10
-################################################################################################################################
-# consine schedule + mse loss + min_snr_5 weight
-torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data' --dataset 'CIFAR-10' \
-          --in_chans 3 --image_size 32 --num_classes 10 --model 'UNet-32' \
-          --lr 1e-4 --betas 0.9 0.999 --dropout 0.1 --drop_label_prob 0.1 --total_steps 200000 --batch_size 128 \
-          --beta_schedule 'cosine' --loss_type 'MSE' --weight_type 'min_snr_5' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp True \
-          --sample_timesteps 10 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 50000 --eval_step 25000 \
-          --fid_cache './stats/fid_stats_cifar_train.npz' 
-
-# linear schedule + mse loss
-torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data' --dataset 'CIFAR-10' \
-          --in_chans 3 --image_size 32 --num_classes 10 --model 'UNet-32' \
-          --lr 1e-4 --betas 0.9 0.999 --dropout 0.1 --drop_label_prob 0.1 --total_steps 200000 --batch_size 128 \
-          --beta_schedule 'linear' --loss_type 'MSE' --weight_type 'constant' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp True \
-          --sample_timesteps 10 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 50000 --eval_step 25000 \
-          --fid_cache './stats/fid_stats_cifar_train.npz' 
-
-# cosine schedule + mse loss
-torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data' --dataset 'CIFAR-10' \
-          --in_chans 3 --image_size 32 --num_classes 10 --model 'UNet-32' \
-          --lr 1e-4 --betas 0.9 0.999 --dropout 0.1 --drop_label_prob 0.1 --total_steps 200000 --batch_size 128 \
-          --beta_schedule 'cosine' --loss_type 'MSE' --weight_type 'constant' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp True \
-          --sample_timesteps 10 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 50000 --eval_step 25000 \
-          --fid_cache './stats/fid_stats_cifar_train.npz' 
-
-# optim schedule + mse loss
-torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data' --dataset 'CIFAR-10' \
-          --in_chans 3 --image_size 32 --num_classes 10 --model 'UNet-32' \
-          --lr 1e-4 --betas 0.9 0.999 --dropout 0.1 --drop_label_prob 0.1 --total_steps 200000 --batch_size 128 \
-          --beta_schedule 'optim_3' --loss_type 'MSE' --weight_type 'constant' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp True \
-          --sample_timesteps 10 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 50000 --eval_step 25000 \
-          --fid_cache './stats/fid_stats_cifar_train.npz' 
-
-# linear schedule + mapped mse loss
-torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data' --dataset 'CIFAR-10' \
-          --in_chans 3 --image_size 32 --num_classes 10 --model 'UNet-32' \
-          --lr 1e-4 --betas 0.9 0.999 --dropout 0.1 --drop_label_prob 0.1 --total_steps 200000 --batch_size 128 \
-          --beta_schedule 'linear' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp True \
-          --sample_timesteps 10 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 50000 --eval_step 25000 \
-          --fid_cache './stats/fid_stats_cifar_train.npz' 
-
-# cosine schedule + mapped mse loss
-torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data' --dataset 'CIFAR-10' \
-          --in_chans 3 --image_size 32 --num_classes 10 --model 'UNet-32' \
-          --lr 1e-4 --betas 0.9 0.999 --dropout 0.1 --drop_label_prob 0.1 --total_steps 200000 --batch_size 128 \
-          --beta_schedule 'cosine' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp True \
-          --sample_timesteps 10 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 50000 --eval_step 25000 \
-          --fid_cache './stats/fid_stats_cifar_train.npz' 
-
-# optim schedule + mapped mse loss
-torchrun --nproc_per_node=2 main.py --train True --eval True --data_dir './data' --dataset 'CIFAR-10' \
-          --in_chans 3 --image_size 32 --num_classes 10 --model 'UNet-32' \
-          --lr 1e-4 --betas 0.9 0.999 --dropout 0.1 --drop_label_prob 0.1 --total_steps 200000 --batch_size 128 \
-          --beta_schedule 'optim_3' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay True --class_cond True --parallel True --amp True \
-          --sample_timesteps 10 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 50000 --eval_step 25000 \
-          --fid_cache './stats/fid_stats_cifar_train.npz' 
-
-
-################################################################################################################################
 # Unsupervised generation in CelebA
 ################################################################################################################################
 
-# CelebA 64×64×3 UNet
+# # CelebA 64×64×3 UNet
+# torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'CelebA' \
+#           --in_chans 3 --image_size 64 --num_classes 0 --model 'UNet-64' \
+#           --lr 1e-4 --betas 0.9 0.999 --dropout 0.0 --drop_label_prob 0.0 --total_steps 500000 --batch_size 128 \
+#           --beta_schedule 'optim_3' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
+#           --warmup_steps 5000 --cosine_decay False --class_cond False --parallel True --amp True \
+#           --sample_timesteps 20 --guidance_scale 1.0 --eps_scaler 1.0 --save_step 100000 --eval_step 100000 --fid_cache '' \
+
+# CelebA 64×64×3 ViT
 torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'CelebA' \
-          --in_chans 3 --image_size 64 --num_classes 0 --model 'UNet-64' \
-          --lr 1e-4 --betas 0.9 0.999 --dropout 0.0 --drop_label_prob 0.0 --total_steps 500000 --batch_size 128 \
+          --patch_size 4 --in_chans 3 --image_size 64 --num_classes 0 --model 'ViT-S' \
+          --lr 1e-3 --betas 0.99 0.99 --dropout 0.0 --drop_label_prob 0.0 --total_steps 500000 --batch_size 128 \
           --beta_schedule 'optim_3' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
           --warmup_steps 5000 --cosine_decay False --class_cond False --parallel True --amp True \
           --sample_timesteps 20 --guidance_scale 1.0 --eps_scaler 1.0 --save_step 100000 --eval_step 100000 --fid_cache '' \
 
-# CelebA 64×64×3 ViT
+# CelebA 64×64×3 DiT
 torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'CelebA' \
-          --patch_size 4 --in_chans 3 --image_size 64 --num_classes 0 --model 'U-ViT-S' \
-          --lr 2e-4 --betas 0.9 0.999 --dropout 0.0 --drop_label_prob 0.0 --total_steps 500000 --batch_size 128 \
+          --patch_size 4 --in_chans 3 --image_size 64 --num_classes 1 --model 'DiT-S' \
+          --lr 1e-3 --betas 0.9 0.999 --dropout 0.0 --drop_label_prob 0.0 --total_steps 500000 --batch_size 128 \
           --beta_schedule 'optim_3' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay False --class_cond False --parallel True --amp True \
+          --warmup_steps 5000 --cosine_decay False --class_cond True --parallel True --amp True \
           --sample_timesteps 20 --guidance_scale 1.0 --eps_scaler 1.0 --save_step 100000 --eval_step 100000 --fid_cache '' \
 
 ################################################################################################################################
