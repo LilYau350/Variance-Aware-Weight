cd6d4c56dead13e3d69496f742589b05ffcf7f38
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2025-04-14 19:38:13 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@
 # torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'CelebA' \
 #           --in_chans 3 --image_size 64 --num_classes 0 --model 'UNet-64' \
 #           --lr 1e-4 --betas 0.9 0.999 --dropout 0.0 --drop_label_prob 0.0 --total_steps 500000 --batch_size 128 \
-#           --beta_schedule 'optim_3' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
+#           --beta_schedule 'cosine' --loss_type 'MSE' --weight_type 'lambda' --sampler_type 'uniform'\
 #           --warmup_steps 5000 --cosine_decay False --class_cond False --parallel True --amp True \
 #           --sample_timesteps 20 --guidance_scale 1.0 --eps_scaler 1.0 --save_step 100000 --eval_step 100000 --fid_cache '' \
 
@@ -16,7 +16,7 @@
 torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'CelebA' \
           --patch_size 4 --in_chans 3 --image_size 64 --num_classes 0 --model 'ViT-S' \
           --lr 1e-3 --betas 0.99 0.99 --dropout 0.0 --drop_label_prob 0.0 --total_steps 500000 --batch_size 128 \
-          --beta_schedule 'optim_3' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
+          --beta_schedule 'cosine' --loss_type 'MSE' --weight_type 'lambda' --sampler_type 'uniform'\
           --warmup_steps 5000 --cosine_decay False --class_cond False --parallel True --amp True \
           --sample_timesteps 20 --guidance_scale 1.0 --eps_scaler 1.0 --save_step 100000 --eval_step 100000 --fid_cache '' \
 
@@ -24,7 +24,7 @@ torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --da
 torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'CelebA' \
           --patch_size 4 --in_chans 3 --image_size 64 --num_classes 1 --model 'DiT-S' \
           --lr 1e-3 --betas 0.9 0.999 --dropout 0.0 --drop_label_prob 0.0 --total_steps 500000 --batch_size 128 \
-          --beta_schedule 'optim_3' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
+          --beta_schedule 'cosine' --loss_type 'MSE' --weight_type 'lambda' --sampler_type 'uniform'\
           --warmup_steps 5000 --cosine_decay False --class_cond True --parallel True --amp True \
           --sample_timesteps 20 --guidance_scale 1.0 --eps_scaler 1.0 --save_step 100000 --eval_step 100000 --fid_cache '' \
 
@@ -32,27 +32,27 @@ torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --da
 # Conditional generation in ImageNet
 ################################################################################################################################
 
-# ImageNet 64×64×3 ViT
-torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'ImageNet' \
-          --patch_size 4 --in_chans 3 --image_size 64 --num_classes 1000 --model 'ViT-L' \
-          --lr 1e-4 --betas 0.99 0.99 --dropout 0.0 --drop_label_prob 0.15 --total_steps 800000 --batch_size 256 \
-          --beta_schedule 'optim_3' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\    
-          --warmup_steps 5000 --cosine_decay False --class_cond True --parallel True --amp True  \
-          --sample_timesteps 20 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 200000 --eval_step 200000 --fid_cache '' \
+# # ImageNet 64×64×3 ViT
+# torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'ImageNet' \
+#           --patch_size 4 --in_chans 3 --image_size 64 --num_classes 1000 --model 'DiT-L' \
+#           --lr 1e-4 --betas 0.99 0.99 --dropout 0.0 --drop_label_prob 0.15 --total_steps 800000 --batch_size 256 \
+#           --beta_schedule 'cosine' --loss_type 'MSE' --weight_type 'constant' --sampler_type 'uniform' --mapping True \    
+#           --warmup_steps 5000 --cosine_decay False --class_cond True --parallel True --amp True  \
+#           --sample_timesteps 20 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 200000 --eval_step 200000 --fid_cache '' \
 
-# ImageNet 32×32×4 UNet
-torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'Encoded_ImageNet' \
-          --in_chans 4 --image_size 32 --num_classes 1000 --model 'LDM' \
-          --lr 1e-4 --betas 0.9 0.999 --dropout 0.0 --drop_label_prob 0.15 --total_steps 800000 --batch_size 256 \
-          --beta_schedule 'optim_3' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
-          --warmup_steps 5000 --cosine_decay False --class_cond True --parallel True --amp True  \
-          --sample_timesteps 50 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 200000 --eval_step 200000 --fid_cache '' \
+# # ImageNet 32×32×4 UNet
+# torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'Encoded_ImageNet' \
+#           --in_chans 4 --image_size 32 --num_classes 1000 --model 'LDM' \
+#           --lr 1e-4 --betas 0.9 0.999 --dropout 0.0 --drop_label_prob 0.15 --total_steps 800000 --batch_size 256 \
+#           --beta_schedule 'cosine' --loss_type 'MSE' --weight_type 'constant' --sampler_type 'uniform' --mapping True \
+#           --warmup_steps 5000 --cosine_decay False --class_cond True --parallel True --amp True  \
+#           --sample_timesteps 50 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 200000 --eval_step 200000 --fid_cache '' \
 
 # ImageNet 32×32×4 ViT
 torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir '' --dataset 'Encoded_ImageNet' \
-          --patch_size 2 --in_chans 4 --image_size 32 --num_classes 1000 --model 'ViT-L' \
+          --patch_size 2 --in_chans 4 --image_size 32 --num_classes 1000 --model 'DiT-B' \
           --lr 1e-4 --betas 0.99 0.99 --dropout 0.0 --drop_label_prob 0.15 --total_steps 800000 --batch_size 256 \
-          --beta_schedule 'optim_3' --loss_type 'MAPPED_MSE' --weight_type 'constant' --sampler_type 'uniform'\
+          --beta_schedule 'cosine' --loss_type 'MSE' --weight_type 'constant' --sampler_type 'uniform' --mapping True \
           --warmup_steps 5000 --cosine_decay False --class_cond True --parallel True --amp True  \
           --sample_timesteps 50 --guidance_scale 1.5 --eps_scaler 1.0 --save_step 200000 --eval_step 200000 --fid_cache '' \
 
