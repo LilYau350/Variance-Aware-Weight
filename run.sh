c16c903f382df80e7cf6d6eb4acb75bf5ecec75d
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2025-04-19 00:14:47 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,17 +8,16 @@ torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir './Cele
           --fid_cache './reference_batches/fid_stats_celeba_train.npz'
 
 # ImageNet 32×32×4 DiT 
-CUDA_VISIBLE_DEVICES=0,1 torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir './ImageNet/ImageNet.h5' --dataset 'Latent' \
-          --patch_size 2 --in_chans 4 --image_size 32 --num_classes 1000 --model 'DiT-B' --mean_type 'EPSILON' \
-          --lr 1e-3 --betas 0.9 0.999 --dropout 0.0 --drop_label_prob 0.15 --total_steps 400000 --batch_size 256 --grad_accumulation 1 \
-          --beta_schedule 'linear' --loss_type 'MSE' --weight_type 'constant' --sampler_type 'uniform' --mapping True \
-          --warmup_steps 5000 --cosine_decay False --class_cond True --parallel True --amp True  --sample_size 16 \
-          --sample_timesteps 50 --guidance_scale 1.5 --sample_step 50000 --num_samples 50000 --save_step 100000 --eval_step 50000 \
-          --fid_cache './reference_batches/VIRTUAL_imagenet256_labeled.npz' 
+CUDA_VISIBLE_DEVICES=0,1 torchrun  --nproc_per_node=2 main.py --train True --eval True --data_dir './ImageNet/ImageNet_256/ImageNet.h5' --dataset 'Latent' \
+          --patch_size 2 --in_chans 4 --image_size 32 --num_classes 1000 --model 'DiT-S' --mean_type 'EPSILON' \
+          --lr 1e-3 --betas 0.9 0.999 --dropout 0.0 --drop_label_prob 0.0 --total_steps 400000 --batch_size 256 --grad_accumulation 1 \
+          --beta_schedule 'cosine' --loss_type 'MSE' --weight_type 'constant' --sampler_type 'uniform' --mapping True \
+          --warmup_steps 0 --cosine_decay False --class_cond True --parallel True --amp True  --sample_size 16 \
+          --sample_timesteps 50 --guidance_scale 1.0 --sample_step 50000 --num_samples 50000 --save_step 100000 --eval_step 50000 \
+          --fid_cache './reference_batches/VIRTUAL_imagenet256_labeled.npz' 
 
 # note 
 # In trainer.py#L76, We can modify trainer.py (autocast()) as with autocast(dtype=torch.bfloat16). BP16 would be more stable than FP16.
 # https://github.com/LilYau350/Representation-Degradation-in-Diffusion-Training/blob/e38244941c7ae726fd0dad8ae2f9b37e95bea78e/tools/trainer.py#L76 
 
 # Here, betas can be set to 0.99 0.99, or 0.9 0.999, we search lr {1e-3,5e-4,1e-4} first. 
-# If loss is always nan in the training process, we can switch beta_schedule 'cosine' to 'linear' 
