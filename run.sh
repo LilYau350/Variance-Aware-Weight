6a7462f3135d76ad562bbb5c2fdefcf397ddf9fc
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2025-04-26 06:40:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=0,1 torchrun  --nproc_per_node=2 main.py --train True --ev
           --beta_schedule 'cosine' --loss_type 'MSE' --weight_type 'constant' --sampler_type 'uniform' --mapping False \
           --warmup_steps 0 --cosine_decay False --class_cond True --parallel True --amp True  --sample_size 16 \
           --sample_timesteps 50 --guidance_scale 1.0 --sample_step 50000 --num_samples 50000 --save_step 100000 --eval_step 50000 \
-          --ref_batch './reference_batches/VIRTUAL_imagenet256_labeled.npz' 
+          --ref_batch './preprocessing/reference_batches/VIRTUAL_imagenet256_labeled.npz' 
 
 # note 
 # In trainer.py#L76, We can modify trainer.py (autocast()) as with autocast(dtype=torch.bfloat16). BP16 would be more stable than FP16.
