620cf7fee5a7ee51d4a191d476f173bfb03b28ea
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2026-02-09 11:43:44 +0800

    Update trainer.py

--- tools/trainer.py
+++ tools/trainer.py
@@ -2,10 +2,11 @@ import torch
 import torch.nn as nn
 from torch.cuda.amp import autocast, GradScaler
 from tools import dist_util, logger
-# from .resample import LossAwareSampler, UniformSampler, create_named_schedule_sampler
-import os, h5py, numpy as np
+import os
 import torch.distributed as dist
-from align_utils import initialize_encoders, get_feature
+from tools.align_utils import initialize_encoders, get_feature
+from collections import OrderedDict
+
 def ema(source, target, decay):
     with torch.no_grad():
         source_dict = source.state_dict()
@@ -21,7 +22,7 @@ def sample_from_latent(latent, latent_scale=1.):
     return latent_samples 
     
 class Trainer:
-    def __init__(self, args, device, model, ema_model, optimizer, scheduler, diffusion, train_loader, start_step, pbar=None):
+    def __init__(self, args, device, model, ema_model, optimizer, scheduler, diffusion, train_loader, pbar=None):
         self.args = args
         self.device = device        
         self.model = model
@@ -31,20 +32,16 @@ class Trainer:
         self.diffusion = diffusion
         self.train_loader = train_loader
         self.datalooper = iter(train_loader)
-        self.encoder = initialize_encoders(args, device) if args.dataset == 'Latent_Pixel' else None
-        # self.schedule_sampler = create_named_schedule_sampler(args.sampler_type, diffusion)
-        self.scaler = GradScaler() if args.amp else None
-        self.start_step = start_step        
+        self.encoder = initialize_encoders(args, device) if args.learn_align else None            
+        self.scaler = GradScaler() if args.amp else None     
         self.pbar = pbar
     
     def _get_next_batch(self):
         try:
             if self.args.dataset == 'Latent_Pixel':
-                # For Feature dataset, return latents, features, labels
                 images, pixels, labels = next(self.datalooper)
                 return images.to(self.device), pixels.to(self.device), labels.to(self.device)
             else:
-                # For other datasets, return images, labels
                 images, labels = next(self.datalooper)
                 return images.to(self.device), labels.to(self.device) if self.args.class_cond else None
         except StopIteration:
@@ -54,7 +51,7 @@ class Trainer:
     def _compute_loss(self, images, labels, features):
         model_kwargs = {"y": labels} if self.args.class_cond else {}
         loss_dict = self.diffusion.training_losses(self.model, images, features, model_kwargs=model_kwargs)   
-        return (loss_dict["loss"]).mean()
+        return loss_dict
     
     def _apply_gradient_clipping(self):
         if self.args.grad_clip:
@@ -63,58 +60,77 @@ class Trainer:
     def _update_ema(self):
         if dist_util.is_main_process():
             ema(self.model, self.ema_model, self.args.ema_decay)
-                   
+                    
     def train_step(self, step):
         self.model.train()
         if self.args.parallel:
             self.train_loader.sampler.set_epoch(step)
         
-        grad_accumulation = max(1, self.args.grad_accumulation)  # Ensure cumulative steps are least 1
-        loss_accumulated = 0.0
+        grad_accumulation = max(1, self.args.grad_accumulation)
+        
+        mse_avg = 0.0
+        align_avg = 0.0
+        total_loss_avg = 0.0
 
         for accumulation_step in range(grad_accumulation):
             features = None 
             if self.args.dataset == 'Latent_Pixel':
-                images, pixels, labels  = self._get_next_batch()
-                features = get_feature(self.args, pixels, self.encoder)
+                images, pixels, labels = self._get_next_batch()
+                if self.args.learn_align:
+                    features = get_feature(self.args, pixels, self.encoder)
             else:
                 images, labels = self._get_next_batch()
-            
+                if self.args.learn_align:
+                    pixels = (images + 1.0) * 127.5
+                    features = get_feature(self.args, pixels, self.encoder)
+                        
             if self.args.in_chans == 4:
                 images = sample_from_latent(images, self.args.latent_scale)  
-                
+            
             if self.args.amp:
                 with autocast():
-                # with autocast(dtype=torch.bfloat16):
-                    loss = self._compute_loss(images, labels, features) / grad_accumulation  # Scale loss for accumulation
+                    loss_dict = self._compute_loss(images, labels, features)
+                    loss = loss_dict["loss"].mean() / grad_accumulation
                 self.scaler.scale(loss).backward()
             else:
-                loss = self._compute_loss(images, labels, features) / grad_accumulation  # Scale loss for accumulation
+                loss_dict = self._compute_loss(images, labels, features)
+                loss = loss_dict["loss"].mean() / grad_accumulation
                 loss.backward()
-                
-            loss_accumulated += loss.item()
 
-            # Perform optimization step only after grad_accumulation steps
+            total_loss_avg += loss.item()
+
+            if "mse" in loss_dict:
+                mse_avg += loss_dict["mse"].mean().item() / grad_accumulation
+            
+            if self.args.learn_align:
+                if "patch" in loss_dict:
+                    patch_avg += loss_dict["align"].mean().item() / grad_accumulation
+
             if (accumulation_step + 1) % grad_accumulation == 0:
                 if self.args.amp:
                     if self.args.grad_clip:
                         self.scaler.unscale_(self.optimizer)
                         self._apply_gradient_clipping()
-                    
                     self.scaler.step(self.optimizer)
                     self.scaler.update()
                 else:
                     self._apply_gradient_clipping()
-                    
                     self.optimizer.step()
                 self.optimizer.zero_grad()
         
-        # Update scheduler
         self.scheduler.step()
         
         if dist_util.is_main_process():
             self._update_ema()
             self.pbar.update(1)
-            self.pbar.set_postfix(loss=loss_accumulated)
+            
+            if self.args.learn_align:
+                display_stats = OrderedDict([
+                    ("align", f"{align_avg:.2f}"),
+                    ("mse", f"{mse_avg:.2}")                                   
+                ])
+                self.pbar.set_postfix(display_stats)
+            else:
+                self.pbar.set_postfix(mse=f"{mse_avg:.4f}")
 
-        return loss_accumulated  # Return scalar accumulated loss 
+        return total_loss_avg
