b345f182d90aaa1fb49705af29476feb4ae58eb2
ZPyAo <121719459+LilYau350@users.noreply.github.com>
2026-02-09 11:25:04 +0800

    Update utils.py

--- tools/utils.py
+++ tools/utils.py
@@ -1,121 +1,202 @@
+import argparse
+import csv
+import sys
+import os
+import re
+import math
+import json
+import yaml 
+import random
 import torch
-import torch.nn as nn
-from torch.cuda.amp import autocast, GradScaler
-from tools import dist_util, logger
-# from .resample import LossAwareSampler, UniformSampler, create_named_schedule_sampler
-import os, h5py, numpy as np
+import numpy as np
+import shutil
+from pathlib import Path
+from datetime import datetime
 import torch.distributed as dist
-from tools.align_utils import initialize_encoders, get_feature
+from torchvision.utils import make_grid, save_image
+from tools import dist_util
+from tools.sampler import Sampler, Classifier
+from tools.trainer import sample_from_latent
+from tqdm import tqdm
+from contextlib import nullcontext
 
-def ema(source, target, decay):
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def snapshot_python_sources(args):
+    project_root = Path(sys.argv[0]).resolve().parent
+    current_logdir = Path(args.logdir).resolve()     
+    logs_root = current_logdir.parent               
+    dst_root = current_logdir / "code"
+
+    for pattern in ("*.py", "*.cpp", "*.cu"):
+        for src in project_root.rglob(pattern):
+            if logs_root in src.parents:
+                continue
+            if "__pycache__" in src.parts: 
+                continue
+            dst = dst_root / src.relative_to(project_root)
+            dst.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy2(src, dst)
+
+    print(f"[snapshot] python sources saved to {dst_root}")
+    
+def generate_logdir(args):
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    logdir = os.path.join(args.logdir, timestamp)
+    args.logdir = logdir
+    if dist_util.is_main_process():       
+        os.makedirs(logdir, exist_ok=True)
+        snapshot_python_sources(args) 
+        config_path = os.path.join(logdir, "config.yaml")
+        with open(config_path, "w") as f:
+            yaml.safe_dump(vars(args), f, sort_keys=False)
+        
+def set_random_seed(args, seed):
+    rank = dist.get_rank() if args.parallel else 0
+    seed = seed + rank
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    
+    
+def get_lr_lambda(args):
+    return lambda step: warmup_cosine_lr(
+        step, args.warmup_steps, args.total_steps,
+        args.lr, args.final_lr, args.cosine_decay)
+
+
+def warmup_cosine_lr(step, warmup_steps, total_steps, lr, final_lr, cosine_decay):
+    if step < warmup_steps:
+        return min(step, warmup_steps) / warmup_steps
+    else:
+        if cosine_decay:
+            progress = (step - warmup_steps) / (total_steps - warmup_steps)
+            cosine_decay = 0.5 * (1 + math.cos(math.pi * progress))
+            return (final_lr + (lr - final_lr) * cosine_decay) / lr
+        else:
+            return 1
+        
+        
+def save_checkpoint(args, step, model, optimizer, ema_model=None):
+    if dist_util.is_main_process():
+        checkpoint_dir = os.path.join(args.logdir, 'checkpoint',)
+        os.makedirs(checkpoint_dir, exist_ok=True)
+        state = {
+            'model': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'step': step
+        }
+        if ema_model is not None:
+            state['ema_model'] = ema_model.state_dict()
+        filename = f"{args.model}_{args.mean_type}_{args.path_type}_{step}.pth"
+        filename = os.path.join(checkpoint_dir, filename)
+        torch.save(state, filename)
+        print(f"Checkpoint saved: {filename}")
+
+def load_checkpoint(ckpt_path, model=None, optimizer=None, ema_model=None):
+    if dist_util.is_main_process():
+        print('==> Resuming from checkpoint..')
+    assert os.path.exists(ckpt_path), 'Error: checkpoint {} not found'.format(ckpt_path)
+    checkpoint = torch.load(ckpt_path, map_location="cpu")
+    if model:
+        model.load_state_dict(checkpoint['model'])
+    if optimizer:
+        optimizer.load_state_dict(checkpoint['optimizer'])
+    if ema_model and 'ema_model' in checkpoint:
+        ema_model.load_state_dict(checkpoint['ema_model'])
+    return checkpoint
+
+
+def generate_samples(args, step, sampler, save_grid=False):
+    """Sample images from the model and either save them as a grid or for evaluation."""
     with torch.no_grad():
-        source_dict = source.state_dict()
-        target_dict = target.state_dict()
-        for key in source_dict.keys():
-            target_dict[key].data.copy_(
-                target_dict[key].data * decay + source_dict[key].data * (1 - decay))
+        all_samples, all_labels = sampler.sample(
+            num_samples=args.num_samples if not save_grid else 64, 
+            sample_size=args.sample_size, 
+            image_size=args.image_size, 
+            num_classes=args.num_classes, 
+            progress_bar=not save_grid,)
+        
+    return save_images(args, step, all_samples,all_labels, save_grid)  
 
-def sample_from_latent(latent, latent_scale=1.):
-    mean, std = torch.chunk(latent, 2, dim=1)
-    latent_samples = mean + std * torch.randn_like(mean)
-    latent_samples = latent_samples * latent_scale 
-    return latent_samples 
-    
-class Trainer:
-    def __init__(self, args, device, model, ema_model, optimizer, scheduler, diffusion, train_loader, start_step, pbar=None):
-        self.args = args
-        self.device = device        
-        self.model = model
-        self.ema_model = ema_model
-        self.optimizer = optimizer
-        self.scheduler = scheduler
-        self.diffusion = diffusion
-        self.train_loader = train_loader
-        self.datalooper = iter(train_loader)
-        self.encoder = initialize_encoders(args, device) if args.dataset == 'Latent_Pixel' else None
-        # self.schedule_sampler = create_named_schedule_sampler(args.sampler_type, diffusion)
-        self.scaler = GradScaler() if args.amp else None
-        self.start_step = start_step        
-        self.pbar = pbar
-    
-    def _get_next_batch(self):
-        try:
-            if self.args.dataset == 'Latent_Pixel':
-                # For Feature dataset, return latents, features, labels
-                images, pixels, labels = next(self.datalooper)
-                return images.to(self.device), pixels.to(self.device), labels.to(self.device)
-            else:
-                # For other datasets, return images, labels
-                images, labels = next(self.datalooper)
-                return images.to(self.device), labels.to(self.device) if self.args.class_cond else None
-        except StopIteration:
-            self.datalooper = iter(self.train_loader)
-            return self._get_next_batch()
+def save_images(args, step, samples, labels, save_grid=False):
+    """Save sampled images as a grid."""
+    if dist_util.is_main_process():
+        arr = np.concatenate(samples, axis=0)
+        arr = arr[: args.num_samples if not save_grid else 64]    
+        
+        if save_grid:
+            # Save as grid image if 'save_grid' is True
+            torch_samples = torch.from_numpy(arr).permute(0, 3, 1, 2).float() / 255.0
+            grid = make_grid(torch_samples, pad_value=0.5)
+            sample_dir = os.path.join(args.logdir, 'sample')
+            os.makedirs(sample_dir, exist_ok=True)
+            path = os.path.join(sample_dir, f'{step}.png')
+            save_image(grid, path)
+        else:
+            # Save for evaluation purposes
+            sample_dir = os.path.join(args.logdir, 'generate_sample')
+            os.makedirs(sample_dir, exist_ok=True)
+            shape_str = "x".join([str(x) for x in arr.shape[1:3]])
+            out_path = os.path.join(sample_dir, f"{args.dataset}_{shape_str}_samples.npz")
             
-    def _compute_loss(self, images, labels, features):
-        model_kwargs = {"y": labels} if self.args.class_cond else {}
-        loss_dict = self.diffusion.training_losses(self.model, images, features, model_kwargs=model_kwargs)   
-        return (loss_dict["loss"]).mean()
+            if args.class_cond:
+                label_arr = np.concatenate(labels, axis=0)[: args.num_samples]
+                np.savez(out_path, arr, label_arr)
+            else:
+                np.savez(out_path, arr)
+            print(f"Evaluation samples saved at {out_path}")
+
+        return arr  # Return the sampled images array for evaluation
+        
+    return None    
+
     
-    def _apply_gradient_clipping(self):
-        if self.args.grad_clip:
-            nn.utils.clip_grad_norm_(self.model.parameters(), self.args.grad_clip)
+def calculate_metrics(args, **kwargs):
+    
+    step, sampler, evaluator, ref_acts, ref_stats, ref_stats_spatial = (
+        kwargs['step'],  kwargs['sampler'], kwargs['evaluator'], 
+        kwargs['ref_acts'], kwargs['ref_stats'], kwargs['ref_stats_spatial'])
+    
+    # Sample images and get the array
+    arr = generate_samples(args, step, sampler)
+    if dist_util.is_main_process():
+        # Calculate metrics if in evaluation mode
+        sample_batch = [np.array(arr[i:i + args.sample_size]) for i in range(0, len(arr), args.sample_size)]
+        sample_acts = evaluator.compute_activations(sample_batch)
 
-    def _update_ema(self):
-        if dist_util.is_main_process():
-            ema(self.model, self.ema_model, self.args.ema_decay)
-                   
-    def train_step(self, step):
-        self.model.train()
-        if self.args.parallel:
-            self.train_loader.sampler.set_epoch(step)
+        sample_stats, sample_stats_spatial = tuple(evaluator.compute_statistics(x) for x in sample_acts)
+        is_score = evaluator.compute_inception_score(sample_acts[0])   
+        fid = sample_stats.frechet_distance(ref_stats)
+        sfid = sample_stats_spatial.frechet_distance(ref_stats_spatial)
+        pre, rec = evaluator.compute_prec_recall(ref_acts[0], sample_acts[0])
+        return is_score, fid, sfid, pre, rec
+    
+    return None, None, None, None, None
+
+def save_metrics_to_csv(args, metrics, step):
+    if dist_util.is_main_process(): 
+        csv_filename = os.path.join(args.logdir, f"metrics.csv")
+        file_exists = os.path.isfile(csv_filename)
         
-        grad_accumulation = max(1, self.args.grad_accumulation)  # Ensure cumulative steps are least 1
-        loss_accumulated = 0.0
+        with open(csv_filename, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            if not file_exists:
+                writer.writerow(['Step'] + list(metrics.keys()))
+            # writer.writerow([step] + list(metrics.values()))
+            formatted_values = [f"{value:.2f}" if isinstance(value, (float, int)) else value for value in metrics.values()]
+            writer.writerow([step] + formatted_values)
 
-        for accumulation_step in range(grad_accumulation):
-            features = None 
-            if self.args.dataset == 'Latent_Pixel':
-                images, pixels, labels  = self._get_next_batch()
-                features = get_feature(self.args, pixels, self.encoder)
-            else:
-                images, labels = self._get_next_batch()
-            
-            if self.args.in_chans == 4:
-                images = sample_from_latent(images, self.args.latent_scale)  
-                
-            if self.args.amp:
-                with autocast():
-                # with autocast(dtype=torch.bfloat16):
-                    loss = self._compute_loss(images, labels, features) / grad_accumulation  # Scale loss for accumulation
-                self.scaler.scale(loss).backward()
-            else:
-                loss = self._compute_loss(images, labels, features) / grad_accumulation  # Scale loss for accumulation
-                loss.backward()
-                
-            loss_accumulated += loss.item()
-
-            # Perform optimization step only after grad_accumulation steps
-            if (accumulation_step + 1) % grad_accumulation == 0:
-                if self.args.amp:
-                    if self.args.grad_clip:
-                        self.scaler.unscale_(self.optimizer)
-                        self._apply_gradient_clipping()
-                    
-                    self.scaler.step(self.optimizer)
-                    self.scaler.update()
-                else:
-                    self._apply_gradient_clipping()
-                    
-                    self.optimizer.step()
-                self.optimizer.zero_grad()
-        
-        # Update scheduler
-        self.scheduler.step()
-        
-        if dist_util.is_main_process():
-            self._update_ema()
-            self.pbar.update(1)
-            self.pbar.set_postfix(loss=loss_accumulated)
-
-        return loss_accumulated  # Return scalar accumulated loss 
